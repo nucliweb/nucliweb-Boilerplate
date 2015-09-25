5b1ddad5da6c6b8b4d22a419b4868c684d4be033
Joan Leon <joan.leon@gmail.com>
2015-09-25 11:32:29 +0200

    Restore backup

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+var gutil = require('gulp-util');
+var iconfont = require('gulp-iconfont');
+var iconfontCss = require('gulp-iconfont-css');
+var fontName = 'Icons';
+
+gulp.task('sass', function() {
+	return gulp.src('scss/style.scss')
+	.pipe(sass().on('error', gutil.log))
+	.pipe(minifyCSS({keepBreaks: false}))
+	.pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', ['sass'], function() {
+  gulp.watch('scss/**/*.scss', ['sass']);
+});
+ 
+gulp.task('iconfont', function(){
+  gulp.src(['images/svg/*.svg']) // svg folder
+    .pipe(iconfontCss({
+      fontName: fontName,
+      path: 'scss/lib/icons-template/_icons.scss', // The template path
+      targetPath: '../scss/lib/_icons.scss', // The path where the (S)CSS file should be saved, relative to the path used in gulp.dest()
+      fontPath: '../../fonts/' // Directory of font files relative to generated (S)CSS file (optional, defaults to ./).
+    }))
+    .pipe(iconfont({
+      fontName: fontName
+     }))
+    .pipe(gulp.dest('fonts/'));
+});
